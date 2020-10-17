3849e6f100f31d0ca736d7ab6bfce88cf9cc620a
Florent Marco <florent.jean.marco@gmail.com>
2020-10-17 15:04:24 +0800

    pushing the code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,9 +14,9 @@ def new
 end
 
 def create
-  byebug
   @task = Task.new(task_params)
   @task.save
+  redirect_to tasks_path(@task)
 end
 
 def edit
@@ -26,11 +26,13 @@ end
 def update
   @task = Task.find(params[:id])
   @task.update(task_params)
+  redirect_to task_path(@task)
 end
 
 def destroy
   @task = Task.find(params[:id])
   @task.destroy
+  redirect_to tasks_path
 end
 
 private
